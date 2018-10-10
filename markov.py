ba4f60e5de550d7b1c9521bea501ac4e9443a2e7
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-09 17:58:13 -0700

    futher study debugging

--- markov.py
+++ markov.py
@@ -2,83 +2,105 @@
 
 from random import choice
 import string 
+import sys 
 
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
 
-	w
+	
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
 
-    # your code goes here
+
+    # open the file with the with block 
     with open(file_path) as file:
     	
+        # method .read() treat the file as one long string  
     	return file.read().strip()
 
+# n give the oppurtunity to add the number of n-gram 
+def make_chains(text_string, n):
 
-def make_chains(text_string):
-
-
+    # create list of words 
     words = text_string.split()
+
+    # append a None to the end of the word list to handle stopping 
     words.append(None)
     
     chains = {}
 
-    for i in range(len(words)-2):
+    # create the tuple of keys , range(len(words)-n >> so it does not 
+    # loop to the end 
+    for i in range(len(words)-n):
         word1 = words[i]
-        word2 = words[i + 1]
+        word2 = words[i + n]
         key = (word1, word2,)
-        value = words[i + 2]
+        value = words[i + n]
 
 
-
+        # initiate the chain keys with an empty list 
         if key not in chains:
             chains[key] = []
 
         chains[key].append(value)
-    #print(chains)
-    # print keys and values in seprate lines 
+
+
+    #print keys and values in seprate lines using items 
     # for key, value in chains.items():
     #     print(key, value)
+
     return chains
 
 
 def make_text(chains):
     """Return text from chains."""
     # picking a random key from our dic, and pull each word out of dic.key()
-    random_key = choice(list(chains.keys()))
-    #print(random_key)
+    
+    # to check if the value ends in punctuation.
+    punct = ([".","?","!"])
+
+
+    # put the keys in a list to have the choice operate on it
+    keys = list(chains.keys())
+
+    # create a random key from list of keys 
+    random_key = choice(keys)
+    
+    # check if the first item if the key is title 
+    while not random_key[0].title():
+        random_key = choice(keys)
 
     # create a list with 2 random keys 
     text = [random_key[0], random_key[1]]
 
     # create a random word from the list of values 
-    random_word = choice(chains[random_key])
-
-
-   
+    random_word = choice(list(chains[random_key]))
 
     # make a loop to keep adding new word from value list to new set of keys 
-    while random_word is not None: 
+    while random_word is not None:
+      
         random_key = (random_key[1],random_word)
         text.append(random_word)
 
-        random_word = choice(chains[random_key])
-
+        if random_word[-1] in punct:
+            break 
+        random_word = choice(keys)
 
 
     return " ".join(text)
 
 
-input_path = "green-eggs.txt"
+input_path = sys.argv[1]
+input_n = int(sys.argv[2])
 
 # Open the file and turn it into one long string
+# enter the sys.argv[1] as the input path 
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text,input_n)
 
 # Produce random text
 random_text = make_text(chains)
